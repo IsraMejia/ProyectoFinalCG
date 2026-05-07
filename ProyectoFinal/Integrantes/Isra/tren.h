53688f648aa4cba5f59e6771d486d074476ea7a7
isra <ihmaisrael@gmail.com>
2026-05-06 21:22:40 -0600

    feat: agrega tren y vagón con estructura jerárquica

--- ProyectoFinal/Integrantes/Isra/tren.h
+++ ProyectoFinal/Integrantes/Isra/tren.h
@@ -23,9 +23,16 @@ public:
 
 private:
 	Model trenModel;
+	Model vagonModel;
 	Material material;
 	glm::vec3 position;
 	glm::vec3 scale;
 	float rotationY;
 	bool initialized;
+	
+	// Offset relativo del vagon respecto al tren (en espacio local del tren)
+	glm::vec3 vagonOffset;
+	
+	// Rotacion adicional del vagon en Y para seguir la curva de las vias
+	float vagonRotationY;
 };
